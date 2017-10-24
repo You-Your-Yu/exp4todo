8b55428b3849de281f2e8403f384ad7558774201
yamada.y.bk <yamada.y.bk@cs-mac57.edu.c.titech.ac.jp>
2017-10-24 11:38:41 +0900

    exp4todo.jsにログインor新規登録の処理を追加

--- public/javascripts/exp4todo.js
+++ public/javascripts/exp4todo.js
@@ -1,3 +1,14 @@
 /* exp4todo.js */
 /* -*- coding: utf-8 -*- */
 
+/* login */
+$(function() {
+	$('.login').on('click', function() {
+		$('#loginForm').action = '/login';
+	});
+	$('.register').on('click', function() {
+		$('#loginForm').action = '/register.html';
+	});
+});
+
+
