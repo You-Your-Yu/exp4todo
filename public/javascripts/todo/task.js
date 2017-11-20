6ad5807de43e354de1a87af0b6e30aab74200294
yu <ya4ma1da3@gmail.com>
2017-11-20 22:54:31 +0900

    [完了する][名前変更][削除する]ボタンを作成(処理は未実装)

--- public/javascripts/todo/task.js
+++ public/javascripts/todo/task.js
@@ -11,10 +11,12 @@ function renderTaskTable(data) {
 
 	$(data).each(function() {
 		var task = this;
+		var script = 'javascript:form' + task.id + '.submit()';
 		var $taskNameAnchor = $('<a></a>').text(task.taskName)
-		.attr('href', '#')
+		.attr('href', script)
 		.addClass('task-name-anchor');
 		var $form = $('<form></form>')
+		.attr('name', 'form' + task.id)
 		.attr('action', 'taskDetail')
 		.attr('method', 'post');
 		$hidden = $('<input>')
@@ -28,14 +30,59 @@ function renderTaskTable(data) {
 		var $picName = $('<td></td>').text(task.picName);
 		var $taskState = $('<td></td>').text(task.taskState);
 		var $limitTime = $('<td></td>').text(task.limitTime);
+		var $btns = createBtns(task.taskState);
+
 		$('<tr></tr>')
 		.addClass('pagination-data')
 		.append($taskName)
 		.append($picName)
 		.append($taskState)
 		.append($limitTime)
+		.append($btns)
 		.appendTo($table);
 	});
+}
+
+function createBtns(taskState) {
+	var script1a = 'completeTaskWithoutST()';
+	var script1b = 'incompleteTaskWithoutST()';
+	var script2 = 'editTaskNameWithoutST()';
+	var script3 = 'delteTaskWithoutST()';
+
+	var ret = $('<td></td>');
+	var ul = $('<ul></ul>').addClass('btns');
+	var li1 = $('<li></li>');
+	var btn1 = $('<button></button>').attr('type', 'button');
+	if(taskState == '未完了') {
+		btn1.text('完了する').addClass('complete-btn').attr('onClick', script1a);
+	}
+	else {
+		btn1.text('完了取消').addClass('incomplete-btn').attr('onClick', script1b);
+	}
+	var li2 = $('<li></li>');
+	var btn2 = $('<button></button>').text('名前変更').addClass('edit-name-btn').attr('type', 'button').attr('onClick', script2);
+	var li3 = $('<li></li>');
+	var btn3 = $('<button></button>').text('削除する').addClass('delete-btn').attr('type', 'button').attr('onClick', script3);
+
+	li1.append(btn1);
+	li2.append(btn2);
+	li3.append(btn3);
+	ul.append(li1).append(li2).append(li3);
+	ret.append(ul);
+	return ret;
+}
+
+function completeTaskWithoutST() {
+	console.log('completeTaskWithoutST');
+}
+function incompleteTaskWithoutST() {
+	console.log('incompleteTaskWithoutST');
+}
+function editTaskNameWithoutST() {
+	console.log('editTaskNameWithoutST');
+}
+function delteTaskWithoutST() {
+	console.log('delteTaskWithoutST');
 }
 
 function filterData(data, filterNames) {
